cd3ea0864a7affd489c93a7cf33d717611808926
Karla Padilla <karlahidalgo740@gmail.com>
2026-04-23 00:23:37 -0600

    Reportes del 1 al 5
    Se crearon los reportes del 1 al 5 y se importo al main el archivo reportes.py y se ademas se le agrego sus respectivos impresiones de datos

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from lectura_datos import cargar_datos  # Importa la función
+from reportes import * #Importa el archivo con la logica de los reportes
 
 # Ruta del archivo
 ruta = "dataset_10000_personas.csv"
@@ -12,3 +13,24 @@ print("Total de registros:", len(datos))
 # Recorre y muestra solo las primeras 2 personas como ejemplo
 for persona in datos[:2]:
     print(persona)
+
+
+print("\n" + "="*70)
+print("REPORTES 1-5".center(70,"-"))
+print("="*70)
+
+#Imprime el resultado obtenido de cada iteracion
+print("Personas por ciudad:")
+print(personas_por_ciudad(datos))
+
+print("\nPersonas por carrera:")
+print(personas_por_carrera(datos))
+
+print("\nPromedio general:")
+print(promedio_general(datos))
+
+print("\nPromedio por carrera:")
+print(promedio_por_carrera(datos))
+
+print("\nPersonas que trabajan:")
+print(personas_que_trabajan(datos)) #Fin de los primeros 5 bloques correspondientes a los primeros 5 reportes
